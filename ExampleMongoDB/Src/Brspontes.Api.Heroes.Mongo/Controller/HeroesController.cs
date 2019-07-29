0fc916bbffc4cc14118ae3d4a4b3b66c41c28359
Brian Pontes <brian.robert16@hotmail.com>
2019-07-28 21:43:38 -0300

    Finalizing CRUD mongodb and .netcore

--- ExampleMongoDB/Src/Brspontes.Api.Heroes.Mongo/Controller/HeroesController.cs
+++ ExampleMongoDB/Src/Brspontes.Api.Heroes.Mongo/Controller/HeroesController.cs
@@ -8,6 +8,7 @@ using Brspontes.Domain.Service.Mongo.HeroesContext.Contract;
 using Brspontes.Domain.Service.Mongo.HeroesContext.Handle;
 using Brspontes.Domain.Service.Mongo.HeroesContext.QueriesResults;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace Brspontes.Api.Heroes.Mongo.Controller
 {
@@ -29,5 +30,21 @@ namespace Brspontes.Api.Heroes.Mongo.Controller
         [Route("heroes")]
         public IEnumerable<GetAllQueryResult> Get() => 
             _heroesHandle.Get();
+
+        [HttpGet]
+        [Route("heroes/{Id}")]
+        public GetByIdQueryResult Get(string Id) => 
+            _heroesHandle.GetById(ObjectId.Parse(Id));
+
+        [HttpPut]
+        [Route("heroes")]
+        public ICommandResult UpdateHero([FromBody] UpdateHeroesCommand command) =>
+            _heroesHandle.Handle(command);
+
+        [HttpDelete]
+        [Route("heroes")]
+        public ICommandResult RemoveHero([FromBody] DeleteHeroCommand command) =>
+            _heroesHandle.Handle(command);
+
     }
 }
